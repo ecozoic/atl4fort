e6dba28e7e24a0dd108cd9e8bae19d44640ff6ae
ecozoic <johnnn.mitchell@gmail.com>
2016-12-09 10:39:58 -0500

    refactor css

--- src/app/components/vote-info/vote-info.jsx
+++ src/app/components/vote-info/vote-info.jsx
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 
 import styles from './vote-info.scss';
 
-import { Button } from 'antd';
-
 /**
  * Vote info component.
  * @extends React.Component
@@ -17,25 +15,38 @@ export class VoteInfo extends Component {
     return (
       <div className={styles.voteInfo}>
         <h2>Voter Info</h2>
-        <p>
-          Election Day: Tuesday, November 7, 2017
-        </p>
-        <p>
-          Polls Open: November 7, 2017 - 07:00am EST
-        </p>
-        <p>
-          Polls Close: November 7, 2017 - 07:00pm EST
-        </p>
+        <div className={styles.table}>
+          <table>
+            <tbody>
+              <tr>
+                <td>Election Day:</td>
+                <td>Tuesday, November 7, 2017</td>
+              </tr>
+              <tr>
+                <td>Polls Open:</td>
+                <td>7:00am EST</td>
+              </tr>
+              <tr>
+                <td>Polls Close:</td>
+                <td>7:00pm EST</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
         <div className={styles.buttons}>
-          <Button type='primary' size='large'>
+          <a href='https://registertovote.sos.ga.gov/GAOLVR/welcome.do#no-back-button' className={styles.button} target='_blank'>
             <i className='fa fa-thumbs-o-up'></i>
             Register to Vote!
-          </Button>
-          <Button type='primary' size='large'>
+          </a>
+          <a href='https://act.myngp.com/Forms/-6565702230050011136' className={styles.button} target='blank'>
             <i className='fa fa-smile-o'></i>
             Volunteer!
-          </Button>
+          </a>
         </div>
+        <p className={styles.hashtag}>
+          <i className='fa fa-hashtag'></i>
+          <span>MayTheFortBeWithYou</span>
+        </p>
       </div>
     );
   }
